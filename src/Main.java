a3f4c3b10e89fd3197ae97338385f82731a947cc
Irene Zhang <irenezy.zhang@gmail.com>
2019-05-06 22:43:52 -0400

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 //Full Name: Kenneth Shinn
@@ -7,19 +8,30 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("*************************************");
-		System.out.println("********NETS 150 FINAL PROJECT*******");
+		System.out.println("***********Wikipedia  Racer**********");
 		System.out.println("*************************************");
 		System.out.println("Welcome to the Wikipedia Racer!");
 		System.out.println("All input links *MUST* be Wikipedia links.");
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter a start link : ");
-		String arg1 = sc.next();
-		System.out.println("Please enter an end link: ");
-		String arg2 = sc.next();
+		String arg1 = "";
+		String arg2 = "";
+		while(sc.hasNext()) {
+			arg1 = sc.next();
+			if(!arg1.contains("https://en.wikipedia.org")) {
+				
+			}
+			System.out.println("Please enter an end link: ");
+			arg2 = sc.next();
 		
-		
-		System.out.println(arg1+ arg2);
+		}
+		sc.close();
+		System.out.println("*************************************");
+		System.out.println("***********Path Results**************");
+		System.out.println("*************************************");
+		List<String> results = BFS.getPath(new PageNode(arg1), new PageNode(arg2));
+		System.out.println(results);
 		
 
 	}
